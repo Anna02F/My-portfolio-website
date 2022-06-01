fd76ef055232411fc9a18bc8894ed9fd98cae2c8
Anna <anna.aghajanyan987@gmail.com>
2022-06-01 12:44:57 -0700

    Remove image from header and change header UI

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,34 +2,36 @@ import React from "react";
 
 import CTALinks from "./CTALinks";
 import HeaderSocials from "./HeaderSocials";
+import { BsArrowDownCircle } from "react-icons/bs";
 import LOGO from "../../assets/logo.svg";
-import Me from "../../assets/me.png";
 
 import "./header.css";
 
 const Header = () => {
     return (
         <header className="header">
-            <a className="logo" href="#">
-                <img src={LOGO}></img>
-            </a>
+            <div className="top-nav">
+                <a className="logo" href="#">
+                    <img src={LOGO}></img>
+                </a>
+                <HeaderSocials />
+            </div>
             <div className="container header__container">
                 <h4>Hello! I'm</h4>
                 <h1>Anna Aghajanyan</h1>
-                <h4>
-                    Frontend/UI developer specializing in developing user
-                    interfaces and responsive design using UI/UX best practices
-                    and research
-                </h4>
+                <h4>Frontend/UI developer</h4>
+                <h5>
+                    Developing user interfaces and responsive design using UI/UX
+                    best practices and research
+                </h5>
                 <CTALinks />
-                <div className="me">
-                    <img src={Me} alt="me" />
-                </div>
-                <div className="header-icons">
-                    <HeaderSocials />
+                <div className="header__scroll">
                     <a href="#portfolio" className="scroll-down">
                         Scroll Down
                     </a>
+                    <div className="down-arrow">
+                        <BsArrowDownCircle />
+                    </div>
                 </div>
             </div>
         </header>
